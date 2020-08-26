6928317489b5b3add261122ea8f2b74f0ea60844
taichan <takecchi.1113@gmail.com>
2020-08-26 23:35:00 +0900

    doing tutorial

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+// Bodyparaser middleware
+app.use(bodyParser.json());
+
